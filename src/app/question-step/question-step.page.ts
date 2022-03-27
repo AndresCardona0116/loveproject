0237fad5754997268be337514518c202b82719d7
PORTMACO1M <portmac01m@MacBook-Pro-de-PORTMACO1M.local>
2022-03-27 01:39:05 -0500

    Andres cardona Descripcion: Steep in 60%

--- src/app/question-step/question-step.page.ts
+++ src/app/question-step/question-step.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertSawalService } from '../services/alertSwal/alert-sawal.service';
 
 @Component({
   selector: 'app-question-step',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionStepPage implements OnInit {
 
-  constructor() { }
+  title: string = 'Pregunta N°1';
+  step: number = 0;
+  formSteps: FormGroup; 
+  response: object = [
+    {
+      'input':  "questionOne", 
+      'question': '¿Cual fue la primera pelicula que vimos en cine?',
+      'response': 'la mujer maravilla'
+    }, 
+    {
+      'input':  "questionTwo", 
+      'question': '¿fecha que viniste a mi casa? (MM/DD/AA)',
+      'response': '12/31/2021'
+    }, 
+    {
+      'input':  "questionThree", 
+      'question': '¿Lugar donde nos dimos nuestro primer beso?',
+      'response': 'un pasillo'
+    }, 
+
+  ];
+  _message: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private alert: AlertSawalService
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(){
+    this.formSteps = this.formBuilder.group({
+			questionOne: [''],
+      questionTwo: [''],
+      questionThree: [''],
+		})
+  }
+
+  validateStep(){
+    if (this.formSteps.controls[this.response[this.step].input].value == '') {
+      this._message = 'Por favor valide todos los campos requeridos para continuar copn la experiencia.';
+      this.alert.errorMessage(this._message); 
+      return
+    }
+    
+    if (this.formSteps.controls[this.response[this.step].input].value.toLowerCase() == this.response[this.step].response ) {
+      if (this.step < 2) {
+        this.step++;
+      }else{
+        alert('fn');
+      }
+    }else{
+      this.alert.messageStep(this.step);
+    }
+    
+
   }
 
 }
--- src/app/services/alertSwal/alert-sawal.service.ts
+++ src/app/services/alertSwal/alert-sawal.service.ts
@@ -6,22 +6,47 @@ import { ToastController } from '@ionic/angular';
 })
 export class AlertSawalService {
 
-  questionOne = [];
+  questionOne = ['1', '2', '3'];
   questionTwo = [];
-  questionthree = [];
+  questionThree = [];
 
   constructor(private toast: ToastController) { }
 
-  async messageStep(question: number, clue: number) {
-    let message = 'hola';
+  async messageStep(question: number) {
+    let message: string;
+    let clue = Math.floor(Math.random() * 3);
+    if (question == 0) {
+      message = this.questionOne[clue];
+    }else if(question == 1){
+      message = this.questionTwo[clue];
+    }else{
+      message = this.questionThree[clue];
+    }
     const toast = await this.toast.create({
         message: message,
-        duration: 6000,
-        position: 'top',
-        color: 'warning'
+        position: 'middle',
+        color: 'info', 
+        buttons: [
+          {
+            text: 'Done',
+            role: 'cancel',
+          }
+        ]
+
     });
     toast.present();
   }
+
+  async errorMessage(message: string) {
+    const toast = await this.toast.create({
+        message: message,
+        duration: 6000,
+        position: 'bottom',
+        color: 'danger'
+    });
+    toast.present();
+  }
+
 }
 
 
